c5b3d50235c45d9d60f899023f55d7bd8a00a58b
0nefroze <dmytro_martynov@icloud.com>
2022-09-26 17:17:24 +0300

    styles.css created to the general CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,17 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}" defer></script>
     <title>Document</title>
 </head>
 <body>
-<h1 class="text-bg-danger">A VERY SOON HERE WILL BE AMAZING WEB SITE MMMMMMM</h1>
+<header>
 
+</header>
+
+<footer>
+
+</footer>
 </body>
 </html>
