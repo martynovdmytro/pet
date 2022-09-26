aa5a648124465c8e11992a565d9372269cee120b
0nefroze <dmytro_martynov@icloud.com>
2022-09-26 16:33:55 +0300

    bootstrap connected

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <title>Document</title>
 </head>
 <body>
-<h1> class="text-bg-danger">A VERY SOON HERE WILL BE AMAZING WEB SITE MMMMMMM</h1>
+<h1 class="text-bg-danger">A VERY SOON HERE WILL BE AMAZING WEB SITE MMMMMMM</h1>
 
 </body>
 </html>
