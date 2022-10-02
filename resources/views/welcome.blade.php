417bb083e424570e198693e38d9468d61ab7217d
0nefroze <dmytro_martynov@icloud.com>
2022-10-02 17:57:34 +0300

    minor fixing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,19 +29,29 @@
         <div class="row col-12 nav-base">
             <div class="row col-5 nav-container">
                 <div class="nav-button">
-                    Graffiti
+                    <button class="button-53">
+                        <h4>Graffiti</h4>
+                    </button>
                 </div>
                 <div class="nav-button">
-                    Street Art
+                    <button class="button-53">
+                        <h4>Street Art</h4>
+                    </button>
                 </div>
                 <div class="nav-button">
-                    Calligraphy
+                    <button class="button-53">
+                        <h4>Calligraphy</h4>
+                    </button>
                 </div>
                 <div class="nav-button">
-                    Sketch
+                    <button class="button-53">
+                        <h4>Sketch</h4>
+                    </button>
                 </div>
                 <div class="nav-button">
-                    Videos
+                    <button class="button-53">
+                        <h4>Videos</h4>
+                    </button>
                 </div>
             </div>
             <div class="col-3 nav-search">
