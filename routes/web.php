1d591060f129c7d8a988b9df18a6903ba3c27108
Dmytro Martynov <dmytro_martynov@icloud.com>
2022-10-16 20:34:53 +0300

    minor fix

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/{any?}', 'welcome', [
+Route::view('/{any?}', 'index', [
     'category' => CategoryController::index()
+]);
+
+Route::view('/admin/admin/{any?}', 'admin', [
+    // TODO: отредактировать линк
+//    'category' => CategoryController::index()
 ]);
