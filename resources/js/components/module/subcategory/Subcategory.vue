7a99f72044e8712eb5aee42f0305377cb2f714bb
Dmytro Martynov <dmytro_martynov@icloud.com>
2022-10-25 14:30:23 +0300

    image carousel html removed to element

--- resources/js/components/module/subcategory/Subcategory.vue
+++ resources/js/components/module/subcategory/Subcategory.vue
@@ -9,20 +9,7 @@
       </div>
     </div>
     <div class="row image-container">
-      <div class="image-carousel col-4">
-        <h5>BIOS</h5>
-        <div class="row col-12">
-          <div class="col-10">
-            TODO: CAROUSEL
-            <div v-for="image in response.data.images" class="col-12 image-carousel-item">
-              <img :src="image.link">
-            </div>
-          </div>
-          <div class="col-2">
-            TODO: SLIDER
-          </div>
-        </div>
-      </div>
+      <image-carousel :images="response.data.images"></image-carousel>
       <div class="image-display col-8">
         TODO: DISPLAY IMAGE
         <div class="image-hashtags col-12">
@@ -34,7 +21,12 @@
 </template>
 
 <script>
+import ImageCarousel from "./template/ImageCarousel";
 export default {
+  components: {
+    ImageCarousel
+
+  },
   props: [
 
   ],
---
+++ resources/js/components/module/subcategory/template/ImageCarousel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="image-carousel col-4">
+    <h5>BIOS</h5>
+    <div class="row col-12">
+      <div class="col-10">
+        TODO: CAROUSEL
+        <div v-for="image in images" class="col-12 image-carousel-item">
+          <img :src="image.link">
+        </div>
+      </div>
+      <div class="col-2">
+        TODO: SLIDER
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+      'images'
+  ],
+}
+</script>
